4cec402e062d5426080460643f79caa6c7421c0b
Nishit Jain <nishitjain1997@gmail.com>
2018-07-07 11:39:59 +0530

    Changes directory structure (js files). Adds form submission and data storage ability (Google script).

---
+++ js/script.js
@@ -0,0 +1,70 @@
+function init() {
+    $("a#a-burger").addClass("burger-left");
+    $("a#a-burger").css('display', 'block');
+    $(".overlay-content").css('height', '0%');
+}
+
+function toggleNav(fsToggle) {
+    if(fsToggle) {
+        $(".overlay").css('height', '0%');
+        fsToggle = false;
+        
+        $(".overlay-content a").each(function() {
+            $(this).css('transition-delay','0ms');
+            $(this).removeClass('vis');
+        });
+        
+        $(".overlay-content").css('height', '0%');
+        
+        $("#hamburger").removeClass('no-bord');
+        $("#hamburger #bar-2").removeClass('hide');
+        $("#hamburger #bar-1").removeClass('turnleft');
+        $("#hamburger #bar-3").removeClass('turnright');
+    }
+    else {
+        $(".overlay").css('height', '100%');
+        fsToggle = true;
+        
+        $(".overlay-content a").each(function(index) {
+            $(this).css('transition-delay',(index * 100) + 'ms');
+            $(this).addClass('vis');
+        });
+        
+        $(".overlay-content").css("height", '');
+        
+        $("#hamburger").addClass('no-bord');
+        $("#hamburger #bar-2").addClass('hide');
+        $("#hamburger #bar-1").addClass('turnleft');
+        $("#hamburger #bar-3").addClass('turnright');
+    }
+    return fsToggle;
+}
+
+$(document).ready(function() {
+    var bannerTop = $("#banner").scrollTop();
+    var bannerBottom = bannerTop + $("#banner").height();
+    var fsToggle = false;
+    
+    init();
+    
+    $("a#a-burger").click(function() {
+        fsToggle = toggleNav(fsToggle);
+    });
+    
+    $(document).scroll(function() {
+        var screenTop = $(document).scrollTop();
+        
+        if(screenTop > bannerBottom - 150) {
+            $("#top").addClass("nav-anime");
+            $("a#a-burger").removeClass("burger-left");
+        }
+        else {
+            $("#top").removeClass("nav-anime");
+            $("a#a-burger").addClass("burger-left");
+            
+            if(fsToggle) {
+                fsToggle = toggleNav(fsToggle);
+            }
+        }
+    });
+});
